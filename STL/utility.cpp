3c4a8cc47c129c5ae5ccffbc7615ce8555e24339
Nikolay <goodsk1llomg@gmail.com>
2020-05-16 21:24:50 +0300

    upd

--- STL/utility.cpp
+++ STL/utility.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
 #include <vector>
 #include <utility>
@@ -8,15 +7,35 @@
 
 /* create overload operator for view result of std::tuple */
 template<typename ...T>
-constexpr std::ostream &operator<<(std::ostream &os, std::tuple<T...> &_tuple) noexcept {
+constexpr std::ostream &operator<<(std::ostream &os, std::tuple<T...> &_tuple) noexcept { //compile time, not trow exceptions
     return os << std::get<0>(_tuple) <<", "<< std::get<1>(_tuple) <<", "<< std::get<2>(_tuple) <<'\n';
 } 
 
 /* create overload operator for view result of std::pair */
 template<typename ...T>
-constexpr std::ostream &operator<<(std::ostream &os, std::pair<T...> &_pair) noexcept {
+constexpr std::ostream &operator<<(std::ostream &os, std::pair<T...> &_pair) noexcept { //compile time, not trow exceptions
     return os << _pair.first << ", "<< _pair.second << '\n';
 }
+
+template <class T>  // lvalue
+T&& go(T& x)
+{
+    std::cout << "Lval this" << '\n';
+    return std::forward<T>(x);
+}
+ 
+template <class T>
+T&& go(T&& x)                  //rvalue
+{
+    std::cout << "Rval this" << '\n';
+    return std::forward<T>(x);
+    /* maybe similarly static_cast<T&&>(x) */
+}
+
+class someType {
+public:
+    int foo () {return 1;}
+};
 
 int main()
 {
@@ -57,6 +76,28 @@ int main()
     bool check = std::is_const_v<decltype(mi)>;
     std::cout << std::boolalpha;
     std::cout << check << '\n';
-    
+
+    /* std::forward */
+    /* Forwards lvalues as either lvalues or as rvalues */
+    /* Need for implementing type template constructor */
+    int lval = 30;
+    int rval = 0;
+    rval = go(lval); // OK, call lval, return lval, used move semantic
+    rval = go(500); // OK, overload function for rval , return rval, used move semantic
+
+    /* small example for use std::declval */
+    decltype(std::declval<someType>().foo()) n2;
+    n2 = 100;
+    std::cout << n2 << '\n';
+
+    /* small example to use std::initializer list */
+    /*  */
+    std::initializer_list<int> ilist = {1, 2, 3, 4, 5};
+    auto alist = {1, 2, 3, 4, 5}; // it's also initializer list
+
+    for(auto &n : ilist){
+        std::cout << n << '\n';
+    } 
+    std::cout << "Initializer list size: " << ilist.size() << '\n';
     return 0;
 }
