44f122a51b85ae89a6b8e2d711a17df95f4561cb
Nikolay <goodsk1llomg@gmail.com>
2020-07-02 16:27:20 +0300

    Update templates.cpp

--- templates.cpp
+++ templates.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <type_traits>
 
 template<typename T1, typename T2>
 auto _add(T1 x, T2 y) -> decltype(x+y){
     return x+y;
 }
 
+template<typename T1, typename T2>
+std::common_type_t <T1, T2> max(T1 a, T2 b){
+    return b < a ? a : b;
+}
+
+template<typename T1, typename T2>
+auto max_v1(T1 a, T2 b) -> typename std::decay_t<decltype(true? a : b)>
+{
+    return b < a ? a : b;
+}
+
 int main()
 {
-    std::cout << _add(5,7) << '\n';
+    std::cout << _add(5, 7.4) << '\n';
+    std::cout << ::max(3.5f, 2) << '\n';
+    std::cout << max_v1(5.6f, 3) << '\n';
     return 0;
 }
